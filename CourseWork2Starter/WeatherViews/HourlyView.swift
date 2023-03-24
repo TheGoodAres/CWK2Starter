614b4e35afe8074a30f62903d4ccd50178f24be6
TheGoodAres <61661335+TheGoodAres@users.noreply.github.com>
2023-03-24 14:59:40 +0000

    refined the UI

--- CourseWork2Starter/WeatherViews/HourlyView.swift
+++ CourseWork2Starter/WeatherViews/HourlyView.swift
@@ -8,28 +8,62 @@
 import SwiftUI
 
 struct HourlyView: View {
-
+    @State var locationString = ""
     @EnvironmentObject var modelData: ModelData
 
     var body: some View {
-        VStack {
-            List {
-                ForEach(modelData.forecast!.hourly) { hour in
-                    HourCondition(current: hour)
-
+//        VStack {
+//            List {
+//                ForEach(modelData.forecast!.hourly) { hour in
+//                    HourCondition(current: hour)
+//
+//
+//                }
+//                .listRowInsets(.init(top: 0, leading: 25, bottom: 0, trailing: 0))
+//                    .listRowBackground(Color.clear)
+//                    .background {
+//                    Color.white
+//                        .opacity(0.1)
+//                }
+//            }
+//            .padding()
+//
+//                .background(Image("background"))
+//                .scrollContentBackground(.hidden)
+//        }
+        ZStack {
+            Image("background")
+                .resizable()
+            VStack {
+                Text(locationString)
+                    .font(.title)
+                List {
+                    ForEach(modelData.forecast!.hourly) { hour in
+                        HourCondition(current: hour)
+                    }
+                        .frame(width: 350)
+                        .listRowInsets(.init(top: 0, leading: 25, bottom: 0, trailing: 0))
+                        .listRowBackground(Color.clear)
+                        .background {
+                        Color.white
+                            .opacity(0.1)
+                    }
 
                 }
-                .listRowInsets(.init(top: 0, leading: 25, bottom: 0, trailing: 0))
-                    .listRowBackground(Color.clear)
+
+                    .padding()
                     .background {
                     Color.white
-                        .opacity(0.1)
+                        .opacity(0.5)
                 }
+                    .scrollContentBackground(.hidden)
             }
-            .padding()
-            
-                .background(Image("background"))
-                .scrollContentBackground(.hidden)
+        }
+        .onAppear {
+            Task.init {
+                self.locationString = await getLocFromLatLong(lat: modelData.forecast!.lat, lon: modelData.forecast!.lon)
+                
+            }
         }
     }
 }
